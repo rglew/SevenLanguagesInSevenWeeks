f1ad74f804e7714b56624eda6c1f075914bca163
richard glew <richard.glew@gmail.com>
2013-02-21 13:51:03 +1000

    day 3 exercises, refined

--- prolog/day2-min.pl
+++ prolog/day2-min.pl
@@ -1,2 +1,4 @@
 minlist([A],A).
 minlist([A|LIST],MIN):- minlist(LIST,MIN0),(A=<MIN0, MIN=A ; A>MIN0, MIN=MIN0).
+
+%-- Usage ?- minlist([2,4,6,8,1],W).
--- prolog/day2-reverse.pl
+++ prolog/day2-reverse.pl
@@ -1,2 +1,4 @@
 reverse([X|Y],Z,W) :- reverse(Y,[X|Z],W).
 reverse([],X,X).
+
+%-- usage ?- reverse([1,2,a,4,5],[],W).
