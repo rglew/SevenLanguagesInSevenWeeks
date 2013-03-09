6b4425a8f716462168e0d19f01b33e5b06021971
Richard <a@a.com>
2013-03-09 19:46:54 +1000

    day2

--- erlang/day2.erl
+++ erlang/day2.erl
@@ -2,7 +2,7 @@
 -compile(export_all).
 
 description(Language) ->
-	LangList = [{erlang, "a functional language"}, {ruby, "an OO language"}, {scala, "a bullshit language"}, {prolog, "a declarative language"}, {c, "a procedural language"},{io, "a prototype language"},{java, "slightly less evil than PHP"},{assembler,"machine language"}],
+	LangList = [{erlang, "a functional language"}, {ruby, "an OO language"}, {scala, "java++"}, {prolog, "a declarative language"}, {c, "a procedural language"},{io, "a prototype language"},{java, "slightly less evil than PHP"},{assembler,"machine language"}],
 	{_,E} = lists:keyfind(Language,1,LangList),
 	E.
 
