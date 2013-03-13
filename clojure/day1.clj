5bc42dbb993dafff60116f55a0033bf7672ec758
richard glew <richard.glew@gmail.com>
2013-03-13 15:54:37 +1000

    clojure day 1

--- clojure/day1.clj
+++ clojure/day1.clj
@@ -1,6 +1,16 @@
 (defn big
-             "Is string st longer than n characters?"
-             [st n]
-             (> (count st) n))
+           [st n]
+           (> (count st) n))
 ; (big "Check out the size of my string" 10)
 ; will return false..
+
+(defn collType
+            [col]
+             ({(class '(1)) :list, (class '()) :list, (class []) :vector, (class {}) :map} (class col)))
+			 
+;Wrote the above then found the below which is a lot cleaner
+(defn collTypeBetter
+			[col]
+              (cond (list? col) :list
+                    (map? col) :map
+                    (vector? col) :vector))
