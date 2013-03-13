22f5ec20ea40c71dd2b68a666372567b4543f8cb
richard glew <richard.glew@gmail.com>
2013-03-13 15:26:00 +1000

    clojure day 1

---
+++ clojure/day1.clj
@@ -0,0 +1,6 @@
+(defn big
+             "Is string st longer than n characters?"
+             [st n]
+             (> (count st) n))
+; (big "Check out the size of my string" 10)
+; will return false..
