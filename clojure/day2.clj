5e344a72209f7ae08ed82320ce6d833916ae0ebe
Richard <a@a.com>
2013-03-16 17:49:06 +1000

    finishing day 2

--- clojure/day2.clj
+++ clojure/day2.clj
@@ -1,3 +1,22 @@
 (defmacro unless
   ([test body] `(if (not ~test) ~body))
   ([test body alt] `(if (not ~test) ~body ~alt)))
+  
+  (defprotocol Printer
+  (print [job device])
+  (cancel_job [job device]))
+  
+  (defrecord HP_Laserjet_1320n []
+  Printer
+  (print [job device]
+    (println (str "Printing " job " on device " device)))
+  (cancel_job [job device]
+    (println (str "Cancelling job " job " on device " device))))
+	
+  (defrecord Canon_Inkjet_600C []
+  Printer
+  (print [job device]
+    (println (str "Printing " job " on device " device)))
+  (cancel_job [job device]
+    (println (str "Cancelling job " job " on device " device))))
+	
